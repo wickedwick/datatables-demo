225749dd34bf21ca2867b325d9ca4f0e3489430e
Travis Wickham <twickham37@gmail.com>
2019-03-27 21:58:53 -0500

    Fixed js errors, added gitignore, eslintrc, readme

---
+++ README.md
@@ -0,0 +1,13 @@
+**Getting Started**
+1. Download and install Node Package manager
+2. Download and install Git
+3. From the command line or terminal, enter the following command:
+   `git clone https://github.com/wickedwick/datatables-demo.git`
+4. Then enter:
+   `cd datatables-demo`
+5. Now install Javascript packages using NPM:
+   `npm intall`
+6. You will also need to install local-web-server globally, run:
+   `npm install -g local-web-server`
+7. Now start your web server:
+   `ws`
--- index.js
+++ index.js
@@ -1,155 +1,82 @@
-$(document).ready(function() {
-  var table = $("#samples").DataTable({
-    processing: true,
-    serverSide: false,
-    pageLength: -1,
-    lengthMenu: [[100, 250, 500, -1], [100, 250, 500, "All"]],
-    ajax: "./data.json",
-    columns: [
-      { data: "", defaultContent: "0", visible: false },
-      {
-        data: "",
-        defaultContent: "",
-        orderable: false,
-        className: "select-checkbox",
-        targets: 1
-      },
-      { title: "Sample Name", className: "dt-left", data: 1 },
-      { title: "Region/Program", className: "dt-left", data: 2 },
-      { title: "Class", className: "dt-left", data: 3 },
-      { title: "Category", className: "dt-left", data: 4 },
-      { title: "QC Concerns", className: "dt-left", data: 5 }
-    ],
-    select: {
-      style: "multi"
-    },
-    order: [[4, "asc"], [5, "asc"], [3, "asc"]],
-    orderFixed: [0, "desc"],
-    dom: "BfrtipF",
-    buttons: [
-      {
-        extend: "excel",
-        text:
-          '<span class="fa fa-file-excel-o"></span> Download (ALL) or (SELECTED)',
-        exportOptions: {
-          columns: [2, 5],
-          modifier: {
-            search: "applied",
-            order: "applied"
-          }
-        }
-      },
-      {
-        text: "Use Selected Library",
-        action: function(e, dt, node, config) {
-          alert(
-            "This buton needs to pass the Sample Name and Category columns to php."
-          );
-        }
-      },
-      {
-        text: "Upload Predefined Library",
-        action: function(e, dt, node, conf) {
-          alert(
-            "This button needs to allow a csv file to be uploaded and passed to php."
-          );
-        }
-      },
-      {
-        text: "Select Default Library 1",
-        action: function(e, dt, node, conf) {
-          alert(
-            "This button will automatically check all rows that match predefined list 1."
-          );
-        }
-      },
-      {
-        text: "Select Default Library 2",
-        action: function(e, dt, node, conf) {
-          alert(
-            "This button will automatically check all rows that match predefined list 2."
-          );
-        }
-      }
-    ]
-  });
+//table source data
+const srcData = [{
+  id: 0,
+  item: 'apple',
+  category: 'fruit'
+}, {
+  id: 0,
+  item: 'banana',
+  category: 'fruit'
+}, {
+  id: 0,
+  item: 'carrot',
+  category: 'veggie'
+}, {
+  id: 0,
+  item: 'raspberry',
+  category: 'berry'
+}, {
+  id: 0,
+  item: 'potato',
+  category: 'veggie'
+}];
 
-  table.on("select", function(e, dt, type, indexes) {
-    if (type === "row") {
-      var row = table.row(dt);
-      table.cell({ row: row.index(), column: 0 }).data("1");
-      table.draw();
-    }
-  });
-
-  table.on("deselect", function(e, dt, type, indexes) {
-    if (type === "row") {
-      var row = table.row(dt);
-      table.cell({ row: row.index(), column: 0 }).data("0");
-      table.draw();
-    }
-  });
-
-  //grab all the unique sorted data entries from the necessary row
-  const category = table
-    .column(4)
-    .data()
-    .unique()
-    .sort();
-
-  //Row click handler
-  $("#samples").on("click", "td", function() {
-    //highlight selected row
-    const clickedTr = $(this).closest("tr");
-
-    //$('#samples tbody tr').removeClass('selected');
-    clickedTr.toggleClass("selected");
-    if (!clickedTr.hasClass("selected")) {
-      writeCell(clickedTr.find("select"));
-    } else {
-      //grab current value of target cell
-      const currentCellValue = table.row(clickedTr).data().category;
-
-      //prepare drop down menu
-      clickedTr
-        .find("td:eq(4)")
-        .html(
-          "<select>" +
-            category.reduce(
-              (options, item) =>
-                (options += `<option value="${item}" ${
-                  item == currentCellValue ? "selected" : ""
-                }>${item}</option>`),
-              ""
-            ) +
-            "</select>"
-        );
-      console.log(
-        "<select>" +
-          category.reduce(
-            (options, item) =>
-              (options += `<option value="${item}" ${
-                item == currentCellValue ? "selected" : ""
-              }>${item}</option>`),
-            ""
-          ) +
-          "</select>"
-      );
-    }
-  });
-
-  //Drop down menu stop event propagation
-  $("#samples").on("click", "tbody td select", event =>
-    event.stopPropagation()
-  );
-
-  //Write dropdown value into table
-  var writeCell = dropdown => {
-    const cellValue = dropdown.val();
-    const currentRow = table.row(dropdown.closest("tr"));
-    const rowData = currentRow.data();
-    rowData.category = cellValue;
-    currentRow.remove();
-    table.row.add(rowData).draw();
-  };
+//DataTable initialization
+const dataTable = $('#samples').DataTable({
+  dom: 't',
+  data: srcData,
+  select: 'multi',
+  columns: Object.keys(srcData[0]).map(key => {
+    return {
+      title: key,
+      data: key
+    };
+  })
 });
+
+//grab all the unique sorted data entries from the necessary row
+const category = dataTable.column(2).data().unique().sort();
+
+//Drop down menu stop event propagation
+$('#samples').on('click', 'tbody td select',
+  event => event.stopPropagation());
+
+//Write dropdown value into table
+const writeCell = dropdown => {
+  const currentRow = dataTable.row(dropdown.closest('tr'));
+  const rowData = currentRow.data();
+  rowData.category = dropdown.val();
+  currentRow.remove();
+  dataTable.row.add(rowData).draw();
+};
+
+dataTable.on('select', function (e, dt, type) {
+  if (type === 'row') {
+    const row = dataTable.row(dt);
+    $(row.node()).find('td:eq(2)').html(
+      '<select>' + category.reduce((options, item) =>
+        options += `<option value="${item}" ${
+          item == row.data().category ? 'selected' : ''}>${
+          item}</option>`, '') + '</select>'
+    );
+    toggleDataAndDraw(row, type, '1');
+  }
+});
+
+dataTable.on('deselect', function (e, dt, type) {
+  if (type === 'row') {
+    const row = dataTable.row(dt);
+    writeCell($(row.node()).find('select'));
+    toggleDataAndDraw(row, type, '0');
+  }
+});
+
+const toggleDataAndDraw = (row, type, dataVal) => {
+  if (type === 'row') {
+    dataTable.cell({
+      row: row.index(),
+      column: 0
+    }).data(dataVal);
+    dataTable.draw();
+  }
+};
